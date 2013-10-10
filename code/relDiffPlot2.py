04e308c28b67363587df8fc5d2e370279aa08ea9
Vedad Hodzic <vedadh@student.matnat.uio.no>
2013-10-10 13:52:18 +0200

    Narrowed down the range of rho_max to more clearly illustrate the differences between the different values.

---
+++ code/relDiffPlot2.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read():
+
+	inFile = open("data/rhoMaxData2.txt", 'r')
+
+	global _rhoMax; _rhoMax = []
+	global _relDiffFirstEig; _relDiffFirstEig = []
+	global _relDiffSecondEig; _relDiffSecondEig = []
+	global _relDiffThirdEig; _relDiffThirdEig = []
+
+	for line in inFile:
+		columns = line.split()
+		_rhoMax.append(float(columns[0]))
+		_relDiffFirstEig.append(float(columns[1]))
+		_relDiffSecondEig.append(float(columns[2]))
+		_relDiffThirdEig.append(float(columns[3]))
+
+	inFile.close()
+
+
+read()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(_rhoMax, _relDiffFirstEig, '.', linestyle='-',
+label='$\lambda = 3$')
+plt.hold('on')
+ax.plot(_rhoMax, _relDiffSecondEig, '.', linestyle='-',
+label='$\lambda = 7$')
+ax.plot(_rhoMax, _relDiffThirdEig, '.', linestyle='-',
+label='$\lambda = 11$')
+plt.hold('off')
+fig.suptitle('Relative difference between numerical and exact eigenvalues', fontsize='14')
+ax.set_xlabel('$\\rho_{max}$', fontsize='16')
+ax.set_ylabel('Relative difference', fontsize='14')
+ax.legend(loc='best')
+plt.grid('on')
+plt.show()
+
+
